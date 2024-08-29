e9d29cadad0f2bc827cccacf67aadc4d6df60fa4
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-08-29 15:22:48 -0400

    added more grid

--- assignment4/src/components/GridPage.vue
+++ assignment4/src/components/GridPage.vue
@@ -47,26 +47,53 @@ const gridTwo = ref([
                         fugiat minus asperiores inventore, nulla harum odio ducimus.`               
     },
 ],);
+const headerThree = ref("Third Grid Container")
+const gridThree = ref([
+    {
+        class: `col-sm-6`,
+        title: `col 1`,
+        body: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt accusamus numquam tenetur 
+                        fugiat minus asperiores inventore, nulla harum odio ducimus.`
+    },
+    {
+        class: `col-sm-6`,
+        title: `col 2`,
+        body: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt accusamus numquam tenetur 
+                        fugiat minus asperiores inventore, nulla harum odio ducimus.`
+    },
+],);
 </script>
 
 <template>
     <main>
-        <div class="row bg-primary"
+        
+        <div class="row bg-primary">
+            <h2>{{headerOne}}</h2>
+            <div class="col-md-3"
             v-for="gOne in gridOne"
             :key="gOne.title">
-            <h2>{{headerOne}}</h2>
-            <div class="col-md-3">
                 <h3 class="text-center">{{gOne.title}}</h3>
                 <p>{{gOne.body}}</p>
             </div>
         </div>
-        <div class="row bg-info"
-            v-for="gTwo in gridTwo"
-            :key="gTwo.title">
+        
+        <div class="row bg-info">
             <h2>{{headerTwo}}</h2>
-            <div :class="gTwo.class">
+            <div :class="gTwo.class"
+                v-for="gTwo in gridTwo"
+                :key="gTwo.title">
                 <h3 class="text-center">{{gTwo.title}}</h3>
                 <p>{{gTwo.body}}</p>
+            </div>
+        </div>
+        
+        <div class="row bg-warning">
+            <h2>{{headerThree}}</h2>
+            <div :class="gThree.class"
+                v-for="gThree in gridThree"
+                :key="gThree.title">
+                <h3 class="text-center">{{gThree.title}}</h3>
+                <p>{{gThree.body}}</p>
             </div>
         </div>
     </main>
