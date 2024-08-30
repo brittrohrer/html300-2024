675eb1bb792032a0e63770ac7ed7cfbee75588df
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-08-30 08:41:14 -0400

    moved component

--- assignment4/src/App.vue
+++ assignment4/src/App.vue
@@ -13,7 +13,7 @@ const routes = {
   "/grid": GridPage,
   "/accordion": AccordionPage,
   "/images": ImagesPage
-};
+}; 
 
 //find current path using the window.location.hash and change the value of hash based on the nav link clicked
 const currentPath = ref(window.location.hash);
@@ -27,30 +27,35 @@ const currentView = computed(() => {
 });
 
 const title =ref(`Website Name`);
+
+const footerTitle = ref (`Footer Info`);
+const footerText= ref(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, incidunt maiores eos quis quo cupiditate 
+            et odit laudantium a impedit nesciunt autem? Exercitationem cum laborum repudiandae animi. Neque, repudiandae 
+            nihil a molestias natus corporis vero culpa praesentium provident optio aspernatur! Accusantium ipsam quis, 
+            voluptas ut laborum perferendis. Quas, quidem modi?`);
+
 </script>
 
 <template>
   <body class="d-flex flex-column min-vh-100">
-  <header class="bg-light">
-    <h1>{{ title }}</h1>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark"> 
-      <ul class="nav navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="#/">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="#/images">Images Page</a></li>
-        <li class="nav-item"><a class="nav-link" href="#/accordion">Accordion Page</a></li>
-        <li class="nav-item"><a class="nav-link" href="#/grid">Grid Page</a></li>
-        <component :is="currentView" />
-      </ul>
-    </nav>
-  </header>
-    <!-- might need to do if else to show correct main content when click -->
-  <footer class="footer bg-light mt-auto">
-            <h4 class="text-center">Footer Info</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, incidunt maiores eos quis quo cupiditate 
-                et odit laudantium a impedit nesciunt autem? Exercitationem cum laborum repudiandae animi. Neque, repudiandae 
-                nihil a molestias natus corporis vero culpa praesentium provident optio aspernatur! Accusantium ipsam quis, 
-                voluptas ut laborum perferendis. Quas, quidem modi?</p>
-  </footer>
+    <header class="bg-light">
+      <h1>{{ title }}</h1>
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark"> 
+        <ul class="nav navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="#/">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="#/images">Images Page</a></li>
+          <li class="nav-item"><a class="nav-link" href="#/accordion">Accordion Page</a></li>
+          <li class="nav-item"><a class="nav-link" href="#/grid">Grid Page</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <component :is="currentView" />
+
+    <footer class="footer bg-light mt-auto">
+      <h4 class="text-center">{{footerTitle}}</h4>
+        <p>{{footerText}}</p>
+    </footer>
   </body>
 </template>
 
